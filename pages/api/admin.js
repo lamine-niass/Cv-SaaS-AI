959a60693e722d8a31031fe389e3c9da94997b58
lamine niass <ml.niass.sn@gmail.com>
2026-07-11 07:07:29 +0000

    Create admin.js

---
+++ pages/api/admin.js
@@ -0,0 +1,26 @@
+export default async function handler(req,res){
+
+  if(req.method !== "POST"){
+    return res.status(405).json({
+      error:"Method not allowed"
+    });
+  }
+
+
+  const { password } = req.body;
+
+
+  if(password !== process.env.ADMIN_PASSWORD){
+
+    return res.status(401).json({
+      error:"Mot de passe incorrect"
+    });
+
+  }
+
+
+  return res.status(200).json({
+    success:true
+  });
+
+}
