d0dc71fd59ae6e58313ca7bbec0420144dd4fd84
lamine niass <ml.niass.sn@gmail.com>
2026-07-11 08:00:29 +0000

    Update send-email.js

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,6 +1,5 @@
 import { Resend } from "resend";
-import { supabase } from "@/lib/supabase";
-
+import { supabase } from '../../lib/supabase';
 
 const resend = new Resend(
   process.env.RESEND_API_KEY
