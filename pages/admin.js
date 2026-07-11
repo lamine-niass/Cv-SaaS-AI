9cb4b292402f8686e7ac405467c735e89600db2c
lamine niass <ml.niass.sn@gmail.com>
2026-07-11 07:06:39 +0000

    Create admin.js

---
+++ pages/admin.js
@@ -0,0 +1,137 @@
+import { useEffect, useState } from "react";
+
+
+export default function Admin(){
+
+  const [orders,setOrders] = useState([]);
+  const [password,setPassword] = useState("");
+  const [logged,setLogged] = useState(false);
+
+
+  async function login(){
+
+    const res = await fetch("/api/admin",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        password
+      })
+    });
+
+
+    if(res.ok){
+      setLogged(true);
+      loadOrders();
+    }
+
+  }
+
+
+
+  async function loadOrders(){
+
+    const res = await fetch("/api/orders");
+
+    const data = await res.json();
+
+    setOrders(data);
+
+  }
+
+
+
+  if(!logged){
+
+    return (
+
+      <div className="card" style={{padding:30}}>
+
+        <h1>
+          🔐 Admin
+        </h1>
+
+        <input
+          className="input"
+          type="password"
+          placeholder="Mot de passe"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+        />
+
+
+        <button
+          className="btn btn-primary"
+          onClick={login}
+        >
+          Connexion
+        </button>
+
+      </div>
+
+    );
+
+  }
+
+
+
+  return (
+
+    <div style={{padding:30}}>
+
+      <h1>
+        📋 Commandes CV
+      </h1>
+
+
+      {
+        orders.map((order)=>(
+
+          <div
+          key={order.id}
+          className="card"
+          style={{
+            padding:20,
+            marginBottom:15
+          }}
+          >
+
+            <h3>
+              {order.name}
+            </h3>
+
+
+            <p>
+              📧 {order.email}
+            </p>
+
+            <p>
+              💳 Transaction :
+              {order.transaction_number}
+            </p>
+
+
+            <p>
+              Offre :
+              {order.offer_label}
+            </p>
+
+
+            <p>
+              Status :
+              {order.status}
+            </p>
+
+
+          </div>
+
+        ))
+      }
+
+
+    </div>
+
+  );
+
+            }
