cb11bf1fb6e151c7ad78755c96116e3db5a43ff6
lamine niass <ml.niass.sn@gmail.com>
2026-07-11 07:07:54 +0000

    Create orders.js

---
+++ pages/api/orders.js
@@ -0,0 +1,43 @@
+import { supabase } from "@/lib/supabase";
+
+
+export default async function handler(req,res){
+
+
+  if(req.method !== "GET"){
+
+    return res.status(405).json({
+      error:"Method not allowed"
+    });
+
+  }
+
+
+
+  const { data, error } =
+    await supabase
+    .from("orders")
+    .select("*")
+    .order(
+      "created_at",
+      {
+        ascending:false
+      }
+    );
+
+
+
+  if(error){
+
+    return res.status(500).json({
+      error:error.message
+    });
+
+  }
+
+
+
+  return res.status(200).json(data);
+
+
+}
